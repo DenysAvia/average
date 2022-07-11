6ed8a099c1ed549627409c3a0420a871d2c5047d
Denys Kovika <d.v.kovika@gmail.com>
2022-07-11 15:48:49 +0300

    ver.1

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -4,9 +4,18 @@ import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         // Use Scanner methods to read input
+        int inp = sc.nextInt();
+        int sum=inp;
+        int count = 0;
+        while (true) {
+            inp = sc.nextInt();
+            if(inp==0) break;
+            sum = inp + sum;
+            count++;}
+        count++;
+        System.out.println(sum/count);
 
-    }
 
-}
+}}
